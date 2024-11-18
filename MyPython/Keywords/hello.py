320b661b2395d8c19efe22d30169d75528370c19
dinakar <dinakartondikatti2@gmail.com>
2024-11-19 00:22:54 +0530

    agian changes made in hello

--- MyPython/Keywords/hello.py
+++ MyPython/Keywords/hello.py
@@ -6,4 +6,9 @@ print(sys.path)
 sum_of_two_variables(5,7)
 
 print("hello how are you")
+print("hi how git workihg")
+print("hi how not workihg")
+print("hi how ghk workihg")
+print("hi how gitsdhs workihg")
+print("hi how ghd9wok workihg")
 
